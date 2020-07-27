ec34306478706f6c04b5f2348dc4062bb39e8a90
Samuel Yoo <samuel.hc.yoo@forso.net>
2020-07-27 02:09:14 -0400

    basic index.html added

---
+++ server.js
@@ -0,0 +1,31 @@
+require( 'dotenv' ).config() // looks for .env ; process.env gets initialized
+
+const express = require("express");
+const exphbs = require("express-handlebars"); 
+const orm = require("./models/orm")
+const routerController = require("./controllers/burgersController");
+
+const app = express();
+// handlebars initialization
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// for parsing incoming POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// for routes
+routerController(app)
+
+// for serving media assets
+app.use( express.static('public') )
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log( `Listening on port: ${PORT}` );
+})
